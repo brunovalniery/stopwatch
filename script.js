6f9f825418fea011b65fc6fd041d7b5a1b7d1f10
Bruno Valniery <brunovalniery@gmail.com>
2023-01-31 22:28:28 -0300

    Adding start and end times as input elements

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-const date = new Date()
-document.getElementById("now").innerHTML = date
-    .toLocaleTimeString()
-    .slice(0, -3)
+const inicio = document.querySelector("start")
+const fim = document.querySelector("end")
 
+inicio.addEventListener("click", add)
+fim.addEventListener("click", add)
+
+// Obtém a data/hora atual
+var data = new Date()
+
+document.getElementById("inicio").innerHTML = getHours() + getMinutes()
+
+// Guarda cada pedaço em uma variável
+var dia = data.getDate() // 1-31
+var dia_sem = data.getDay() // 0-6 (zero=domingo)
+var mes = data.getMonth() // 0-11 (zero=janeiro)
+var ano2 = data.getYear() // 2 dígitos
+var ano4 = data.getFullYear() // 4 dígitos
+var hora = data.getHours() // 0-23
+var min = data.getMinutes() // 0-59
+var seg = data.getSeconds() // 0-59
+var mseg = data.getMilliseconds() // 0-999
+var tz = data.getTimezoneOffset() // em minutos
+
+// Formata a data e a hora (note o mês + 1)
+var str_data = dia + "/" + (mes + 1) + "/" + ano4
+var str_hora = hora + ":" + min + ":" + seg
+
+function add() {
+  var msghi = "Início: " + str_hora
+  document.getElementById("start").value = msghi
+  var msghf = "Fim: " + str_hora
+  document.getElementById("end").value = msghf
+}
